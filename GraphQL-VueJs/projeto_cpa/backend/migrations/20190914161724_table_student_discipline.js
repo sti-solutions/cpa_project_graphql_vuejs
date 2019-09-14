6942e674a375bf5e2972c153b7c12d63b40b9e64
Iago <iagofalcom@hotmail.com>
2019-09-14 17:08:00 -0300

    Tables students_disciplines e answers

--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914161724_table_student_discipline.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914161724_table_student_discipline.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('students_disciplines',table=>{
+        table.increments('id').primary()
+        table.string('students_id').notNull()
+        table.string('disciplines_id').notNull()
+    })
   
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTable('students_disciplines')
   
 };
--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914161939_table_answer.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914161939_table_answer.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('answers',table=>{
+        table.increments('id').primary()
+    })
   
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTable('answers')
   
 };
