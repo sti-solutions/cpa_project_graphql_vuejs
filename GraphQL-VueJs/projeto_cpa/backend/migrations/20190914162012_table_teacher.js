0ab1d51b095228e79e3baa5740aeb7a390e6da1c
Iago <iagofalcom@hotmail.com>
2019-09-14 16:49:17 -0300

    Table teacher

--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162012_table_teacher.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162012_table_teacher.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
+
+    return knex.schema.createTable('teachers',table=>{
+        table.increments('id').primary()
+        table.string('name').notNull().unique()
+        table.string('email').notNull()
+        table.string('mobile').notNull()
+        table.string('company_id').notNull()
+    })
   
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('teachers')
 };
