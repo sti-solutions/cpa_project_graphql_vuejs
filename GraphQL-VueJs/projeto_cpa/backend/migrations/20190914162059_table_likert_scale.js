fe66f209765b218e0a5e58f40ff415f32c876fac
Iago <iagofalcom@hotmail.com>
2019-09-14 17:11:52 -0300

    Tables options e likert_scale

--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162059_table_likert_scale.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162059_table_likert_scale.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('likert_scale',table=>{
+        table.increments('id').primary()
+        table.string('description').notNull()
+        
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('likert_scale')
 };
--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162108_table_option.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162108_table_option.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('options',table=>{
+        table.increments('id').primary()
+        table.string('description').notNull()
+        
+    })
   
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTable('options')
   
 };
