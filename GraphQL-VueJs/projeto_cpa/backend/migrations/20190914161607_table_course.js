578fb369cc301f4d31d0d94685adf60fd1e2f714
Iago <iagofalcom@hotmail.com>
2019-09-14 16:51:23 -0300

    Table courses

--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914161607_table_course.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914161607_table_course.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('courses',table=>{
+        table.increments('id').primary()
+        table.string('name').notNull().unique()
+        table.string('company_id').notNull()
+    })
   
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTable('courses')
   
 };
