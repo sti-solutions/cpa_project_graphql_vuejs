3aa907698ca68da7cbc65280837b25d0e609f890
Iago <iagofalcom@hotmail.com>
2019-09-14 17:03:10 -0300

    Table questions

--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162042_table_question.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162042_table_question.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('questions',table=>{
+        table.increments('id').primary()
+        table.string('title').notNull()
+        table.string('description').notNull()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('questions')
 };
