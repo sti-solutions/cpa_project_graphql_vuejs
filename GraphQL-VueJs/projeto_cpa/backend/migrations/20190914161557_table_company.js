97e74ed8b4723d48532ca14f87d2266e1b7aa645
Iago <iagofalcom@hotmail.com>
2019-09-14 16:43:13 -0300

    Table company

--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914161557_table_company.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914161557_table_company.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('companys',table=>{
+        table.increments('id').primary()
+        table.string('name').notNull().unique()
+        table.string('email').notNull()
+        table.string('address').notNull()
+        table.string('phone').notNull()
+        table.string('mobile').notNull()
+        table.string('company_type_id').notNull()
+    })
   
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTable('companys')
   
 };
