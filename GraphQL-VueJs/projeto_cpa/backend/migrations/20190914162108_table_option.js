cd0990b279101850fd15d6cec5a65ceff0225c15
Iago <iagofalcom@hotmail.com>
2019-09-14 21:34:03 -0300

    Relacionamento opções

--- GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162108_table_option.js
+++ GraphQL-VueJs/projeto_cpa/backend/migrations/20190914162108_table_option.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('options',table=>{
         table.increments('id').primary()
         table.string('description').notNull()
+        table.integer('likert_scale_id').unsigned().references('likert_scale.id');
         table.timestamps(true,true)
         
     })
